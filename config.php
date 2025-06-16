f812464d7a26d55752e6b320ab75795ce06dab7e
Amit Das <112541611+AmitDas4321@users.noreply.github.com>
2025-06-17 02:00:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 define('PINTEREST_API_BASE', 'https://api.amitdas.site/Pinterest/api/');
 
 // WhatsApp API config
-define('WHATSAPP_INSTANCE_ID', 'YOUR_INSTANCE_ID');
-define('WHATSAPP_ACCESS_TOKEN', 'YOUR_ACCESS_TOKEN');
+define('TEXTSNAP_INSTANCE_ID', 'YOUR_INSTANCE_ID');
+define('TEXTSNAP_ACCESS_TOKEN', 'YOUR_ACCESS_TOKEN');
