23c1368cb5a32e7954990af031ce9166d7c6ec4e
Amit Das <112541611+AmitDas4321@users.noreply.github.com>
2025-10-13 15:25:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Pinterest downloader API base URL (without "?url=")
-define('API_BASE', 'https://api.amitdas.site/Pinterest/api/');
+define('API_BASE', 'https://wadownloader.amitdas.site/api/');
 
 // WhatsApp API config
 define('WHATSAPP_INSTANCE_ID', 'YOUR_INSTANCE_ID');
